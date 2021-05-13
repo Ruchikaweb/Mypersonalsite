a3f8cc675593a3d0b21a753335e87b19fe916b81
Ruchikaweb <ruchikanikita94135@gmail.com>
2021-05-13 13:43:21 +0530

    aa

---
+++ src/component/addminstrator/try.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Add extends React.Component{
+    constructor(){
+
+    }
+    render(){
+        return(
+            <>
+                
+            </>
+        )
+    }
+}
+
+export default Add
